7d3369f4ea98217be7bd25cca61f36afc6df7225
InGee Kim <ingee@paran.com>
2016-12-02 08:08:11 +0900

    test utf8 string

---
+++ ch06/file_ops_utf8.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+  cout << "hello, " << argv[0] << "\n";
+  cout << "what is different between a UTF-8 string written in source code\n";
+  cout << " and a UTF-8 string read from UTF-8 text file?\n";
+  cout << "\n";
+
+  cout << "bye, " << argv[0] << "\n";
+}
