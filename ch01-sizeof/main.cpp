a8b7fffa95d2ce9c51282d75394480da580ca20f
SW-Defined Infra Lab 김인기 <ingee.kim@sk.com>
2017-11-08 08:21:44 +0900

    clean up

--- ch01-sizeof/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  std::cout << "sizeof(int) = " << sizeof(int) << ", "
-    << "sizeof(float) = " << sizeof(float) << ", "
-    << "sizeof(double) = " << sizeof(double) << "\n";
-}
